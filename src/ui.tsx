d223d0464aa4515b9fa5149b4e89c8e862731437
Eric Matte <eric.matte@hotmail.ca>
2020-09-19 23:49:50 -0400

    Add scripts and playground

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Text } from "ink";
+import { Box, Text, useFocus } from "ink";
 
 interface Props {
   name?: string;
 }
 
-export const App: React.FC<Props> = ({ name }: Props): React.ReactElement => (
-  <Text>
-    Hello, <Text color="green">{name}</Text>
-  </Text>
-);
+export const App = ({ name = "Stranger" }: Props): React.ReactElement => {
+  const { isFocused } = useFocus();
+  const { isFocused: is2 } = useFocus();
 
-App.defaultProps = {
-  name: "Stranger",
+  return (
+    <>
+      <Text>{isFocused ? "I am focused" : "I am not focused"}</Text>
+      {/* <Text>
+      Hello, <Text color="green">{name}</Text>
+    </Text> */}
+      <Box borderStyle="round" borderColor="green">
+        <Text>Green Rounded Box</Text>
+      </Box>
+      <Text>{is2 ? "I am too focused" : "I am not too focused"}</Text>
+    </>
+  );
 };
